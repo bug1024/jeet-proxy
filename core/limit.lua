f8e764faa20f8773a877b7ad52a559012722ad7e
bug1024 <bug1024.com@gmail.com>
2017-05-04 23:22:31 +0800

    rate limit

--- core/limit.lua
+++ core/limit.lua
@@ -2,10 +2,30 @@
 local M = {}
 M.version = "0.0.1"
 
-M.get_cache_key = function()
+local redis = require "resty.redis"
+local red = redis:new()
+red:set_timeout(1000)
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.log(ngx.ERR, "failed to connect: ", err)
+    return
 end
 
-M.incr = function()
+M.get_config = function(api)
+    local config = {}
+    return config
+end
+
+M.get_cache_key = function(api)
+    local cache_key = ""
+    return cache_key
+end
+
+M.incr = function(api)
+    local config = self.get_config(api)
+    local cache_key = self.get_cache_key(api)
+    local count, err = red:incr()
+    -- TODO
 end
 
 return M
